e969ff09e9625f07c85b6963628837b3a3ce0c1d
Arjan Egges <arjan@arjancodes.com>
2026-05-21 13:38:01 +0200

    Added resources and async code examples.

---
+++ 6_system_level_3/2_resources/synchronization_lock_api.py
@@ -0,0 +1,32 @@
+# synchronization_lock_api.py
+
+from collections.abc import Iterator
+from contextlib import contextmanager
+from threading import Lock
+
+sync_lock = Lock()
+
+
+@contextmanager
+def synchronization_lock() -> Iterator[None]:
+    print("Acquiring synchronization lock")
+    sync_lock.acquire()
+
+    try:
+        yield
+    finally:
+        print("Releasing synchronization lock")
+        sync_lock.release()
+
+
+def synchronize_bank_import() -> None:
+    print("Synchronizing transactions from bank API")
+
+
+def run_scheduled_sync() -> None:
+    with synchronization_lock():
+        synchronize_bank_import()
+
+
+if __name__ == "__main__":
+    run_scheduled_sync()
